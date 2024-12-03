d036a917a49baa48076f286278519434b02b1a36
miikanoff <visualneli4@gmail.com>
2024-12-03 14:48:30 +0100

    Operaciones.java creado

---
+++ src/Operaciones.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Operaciones {
+    public static int Multiplicar(int x, int y) {
+
+        return x * y;
+    }
+    public static int Dividir(int x, int y) {
+
+        return x / y;
+    }
+}
