1d89ef005feb2630d779baf9b92d7b3b238d4a68
miikanoff <visualneli4@gmail.com>
2024-12-03 14:55:37 +0100

    Añadidas funciones de bienvenida y despedida

---
+++ src/MensajeOperaciones.java
@@ -0,0 +1,11 @@
+package src;
+public class MensajeOperaciones {
+    public static void Bienvenida() {
+        System.out.println("Bienvenido");
+        
+    }
+    public static void Despedida() {
+        System.out.println("Hasta luego");
+        
+    }
+}
